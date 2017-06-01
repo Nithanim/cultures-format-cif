a3cdaa3725f037ba77aa0843bc0507f9aa56023f
Nithanim <git@nithanim.me>
2017-06-01 21:14:10 +0200

    Added encryption function

--- src/main/java/me/nithanim/cultures/format/cif/EncryptionUtil.java
+++ src/main/java/me/nithanim/cultures/format/cif/EncryptionUtil.java
@@ -23,6 +23,26 @@ public class EncryptionUtil {
         }
     }
 
+    public static void encryptCommon(byte[] decrypted) {
+        int c = 71;
+        int d = 126;
+        for (int i = 0; i < decrypted.length; i++) {
+            decrypted[i] = (byte) (((decrypted[i] & 0xFF) ^ c) + 1);
+            c += d;
+            d += 33;
+        }
+    }
+
+    public static void encryptCommon(ByteBuf src, ByteBuf dest) {
+        int c = 71;
+        int d = 126;
+        while (src.readableBytes() > 0) {
+            dest.writeByte((byte) (((src.readByte() & 0xFF) ^ c) + 1));
+            c += d;
+            d += 33;
+        }
+    }
+
     private EncryptionUtil() {
     }
 }
